a197c9182c4b46bf33d339b4c4a95ace9b6c739a
Stream0123 <158164010+Stream0123@users.noreply.github.com>
2026-04-28 17:36:24 +0530

    Update mpdstream.js

--- mpdstream.js
+++ mpdstream.js
@@ -3,12 +3,12 @@ const STREAMS = {
     mpd: "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/tll6uwepxa/out/v1/a7f67cbb33df46539312956427343886/cenc.mpd",
     keyId: "290e09c837da78d5cd961978d390515c",
     key: "b748836c71e6a4ca68ef5b5652db6247",
-    poster: "https://linear-poster.astro.com.my/prod/poster/Astro-Cricket_2024_IVP_LAND_917x516.jpg"
+    poster: "#"
   },
   cricbuzz: {
     mpd: "https://ev-fast-mpd.starzplayarabia.com/live/eds/CricLife1/DASH/CricLife1.mpd",
     keyId: "1e23e51b09a67b83813c17049870bcd4",
     key: "c3d6288e378b0dc55320fd0e6fd2e55f",
-    poster: "https://via.placeholder.com/1280x720.png?text=Stream+2"
+    poster: "#"
   }
 };
