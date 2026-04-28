461054971b04463eb7858fb4fac2c0ffbc0215a8
Stream0123 <158164010+Stream0123@users.noreply.github.com>
2026-04-28 13:25:40 +0530

    Create mpdstream.js

---
+++ mpdstream.js
@@ -0,0 +1,15 @@
+const STREAMS = {
+  WillowSports: {
+    mpd: "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/tll6uwepxa/out/v1/a7f67cbb33df46539312956427343886/cenc.mpd",
+    keyId: "290e09c837da78d5cd961978d390515c",
+    key: "b748836c71e6a4ca68ef5b5652db6247"
+  },
+
+  M2: {
+    mpd: "https://example.com/stream2.mpd",
+    keyId: "abcd1234abcd1234abcd1234abcd1234",
+    key: "1234567890abcdef1234567890abcdef"
+  }
+
+  // add unlimited streams here
+};
