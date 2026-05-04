d3af0ceeb5746688d9280e42824c177b991831ee
Stream0123 <158164010+Stream0123@users.noreply.github.com>
2026-05-04 19:21:26 +0530

    Update mpdstream.js

--- mpdstream.js
+++ mpdstream.js
@@ -6,9 +6,9 @@ const STREAMS = {
     poster: "#"
   },
   IPL: {
-    mpd: "https://otte.cache.aiv-cdn.net/iad-nitro/live/clients/dash/enc/adb04xbxx2/out/v1/cc995fdb08e045308b91b144b3acc5a6/cenc.mpd",
-    keyId: "82eabe094566556d22f1708a1b466113",
-    key: "8ea1e0002435567dd51ef8c848f65bbe",
+    mpd: "https://otte.cache.aiv-cdn.net/iad-nitro/live/clients/dash/enc/d1szunmd7y/out/v1/8ea178a462f24ec09879efbeda25a06e/cenc.mpd",
+    keyId: "38fa07d67a407ee3abd04c0d52f6a751",
+    key: "7433bad605361863a2f145eb2e40e074",
     poster: "#"
   }
 };
